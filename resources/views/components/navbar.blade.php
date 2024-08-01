550c7941d099f9a8f1a733b92dd01bc16beef0cd
Mythrand <jakub.kajanovic1@gmail.com>
2024-08-01 21:55:54 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
-<ul class="flex flex-row lg:gap-6 md:gap-6 gap-4 lg:text-md md:text-md text-sm p-2">
+<ul class="flex flex-row lg:gap-6 md:gap-6 gap-4 lg:text-md md:text-md text-sm pr-3">
     <li><a href="/">@lang('messages.domov')</a></li>
     <li><a href="{{route('blog')}}">@lang('messages.blog')</a></li>
-    <li><a href="">Messin App</a></li>
+    <li class="text-center"><a href="">Messin App</a></li>
     <li><a href="{{route('contact')}}">@lang('messages.kontakt')</a></li>
     <li>
         <x-language/>
