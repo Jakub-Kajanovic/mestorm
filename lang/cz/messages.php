05ca4a42b126220c44e8631cb04938b21b8143a9
Mythrand <jakub.kajanovic1@gmail.com>
2024-08-07 19:39:33 +0200

    lang for cz.

---
+++ lang/cz/messages.php
@@ -0,0 +1,114 @@
+<?php 
+
+return [
+    /* nav bav */
+    'localization' => 'Lokalizace',
+    'domov' => 'Domov',
+    'blog' => 'Blog',
+    'kontakt' => 'Kontakt',
+    'Vyžiadať is cenovú ponuku' => 'Požádat o cenovou nabídku',
+    'price-offer-white' => 'Požádat o cenovou nabídku',
+    'jazyk' => 'Jazyk',
+    /*logo */
+    'logo-text' => 'Zařízení pro strojírenství od firmy Metales',
+    /* home */
+    'ms-s-home' => 'Odsávání aerosolů procesních kapalin
+                (olejové mlhy)',
+    'ms-supervisor-home' => 'Dokonalý výhled operátora na CNC
+                nástroj a obrobek během obrábění',
+    'ms-waste-home' => 'Inteligentní a snadná likvidace
+                odpadu',
+    'ms-dust-home' => 'Odsávání prachu a nečistot 
+                z kompresoroven a technických místností',
+    /* ms-aerosol*/
+    'ms-aerosol-title' => 'Odsávače aerosolů procesních kapalin',
+    'ms-aerosol-text' => 'Odsávač aerosolů procesních kapalin Mestorm Solution zbaví vaši provozovnu olejové mlhy a problémů s ní souvisejících.',
+    'ms-aerosol-why-areosol' => 'Proč používat odsávače Mestorm Solution?',
+    'ms-aerosol-health' => 'Ochrana zdraví',
+    'ms-aerosol-health-text' => 'Dobří zaměstnanci jsou největší devízou každé výrobní společnosti. Odsávače Mestorm Solution přímo chrání zdraví lidí zaměstnaných v kovozpracujícím a strojírenském průmyslu, když minimalizují jejich kontakt s olejovou mlhou.',
+    'ms-aerosol-safe' => 'Úspora řezné kapaliny',
+    'ms-aerosol-safe-text' => 'Signifikantní podíl odsávaných částic procesních kapalin rozptýlených v olejové mlze se sráží na vnitřních stěnách odsávače i odsávací hadice, vrací se zpět do systému CNC stroje a Mestorm Solution tak šetří vaše náklady.',
+    'ms-aerosol-assembly' => 'Snadná montáž',
+    'ms-aerosol-assembly-text' => 'Při koupi odsávače Mestorm Solution zákazníkovi nabízíme i odbornou montáž. Není však žádný problém, pokud se zákazník rozhodne instalovat odsávač sám. Součástí balení je kompletní montážní materiál a podrobný návod.',
+    'ms-aerosol-clean' => 'Čisté pracoviště',
+    'ms-aerosol-clean-text' => 'Mestorm Solution odsává olejovou mlhu z CNC stroje, odkud by se za jiných okolností po otevření stroje dostávala do volného prostoru a částice z ní by se usazovaly na podlaze, stěnách, stropech a ostatních zařízeních vaší haly.',
+    'ms-aerosol-long-reability' => 'Dlouhodobá spolehlivost',
+    'ms-aerosol-long-reability-text' => 'Výrobce odsávačů Mestorm Solution, Metales s. r. o. má všechny své CNC stroje vybavené odsávači MS již několik let. Měření prokázala jejich výbornou efektivitu v náročných podmínkách nepřetržité výroby.',
+    'ms-aerosol-easy-maintenance' => 'Nenáročná údržba',
+    'ms-aerosol-easy-maintenance-text' => 'Údržba odsávačů Mestorm Solution spočívá v pravidelných výměnách filtrů. Vyjmutí starého a osazení nového vnitřního i vnějšího filtru je velmi jednoduché a zabere maximálně pár minut.',
+    'ms-aerosol-filter' => 'Termíny výměn filtrů za vás hlídáme my',
+    'ms-aerosol-filter-text1' => 'Podmínkou správného fungování odsávačů je výměna vnitřního filtru I. každých 120 dní a vnějšího filtru II. každých 365 dní. Majitel odsávačů tyto termíny nemusí nijak hlídat. Každý odsávač je pod svým ID číslem registrován v databázi výrobce. Ta obsahuje všechny údaje o výrobě, expedici, montáži, servisní historii odsávače a hlavně, životnosti osazených filtrů.',
+    'ms-aerosol-filter-text2' => 'Díky naší databázi odsávačů zákazníka s dostatečným předstihem upozorníme na blížící se termín výměny a včas filtry odešleme.',
+    'ms-aerosol-filter-text3' => 'K údajům z databáze se může kdykoliv jednoduše dostat i zákazník. Stačí smartphonem s připojením na internet oskenovat QR kód umístěný na odsávači. Kromě zmíněných informací tak klient získá i přístup k různým dokumentům, návodům či objednávkovým formulářům.',
+    'ms-aerosol-information' => 'Všechny informace okamžitě k dispozici',
+    'ms-aerosol-information-text1' => 'Každý odsávač je označen unikátním QR kódem',
+    'ms-aerosol-information-text2' => 'Naskenujte QR kód přes vhodnou aplikaci ve smartphonu s přístupem na internet',
+    'ms-aerosol-information-text3' => 'Nyní máte přístup k mnoha informacím o vašem odsávači',
+    'ms-aerosol-information-text4' =>   'Výrobní číslo (ID) odsávače.
+                                        Údaje o výrobě, balení a expedici.
+                                        Doba používání filtrů.
+                                        Zbývající životnost filtrů.
+                                        Návody na montáž a obsluhu odsávače.
+                                        Návod na výměnu filtrů.
+                                        Požadavek na servis a objednávkový formulář.
+                                        Podrobnosti o aktuální instalaci odsávače.
+                                        Kompletní servisní historie',
+    'ms-aerosol-solutions' => 'Konkrétní řešení pro každý stroj',
+    'ms-aerosol-solutions-text' => 'Odsávač Mestorm Solution se vyrábí ve třech velikostech s různě dimenzovaným výkonem. Tak, aby vyhovoval širokému spektru CNC strojů působících v kovozpracujícím průmyslu.',
+    'ms-aerosol-ms-s' => 'Odsávač MS-S',
+    'ms-aerosol-ms-s-text' => 'Určený pro stroje s pracovním prostorem menším než 1,5 m3',
+    'ms-aerosol-ms-m' => 'Odsávač MS-M',
+    'ms-aerosol-ms-m-text' => 'Určený pro stroje s pracovním prostorem od 1,5 m3 do 3 m3',
+    'ms-aerosol-ms-l' => 'Odsávač MS-L',
+    'ms-aerosol-ms-l-text' => 'Určený pro stroje s pracovním prostorem větším než 3 m3',
+    'ms-aerosol-maker' => 'Odsávač Mestorm Solution vyrábí Metales, s. r. o.',
+    'ms-aerosol-maker-text' => 'Naše společnost od roku 1993 úspěšně podniká v oblasti kovovýroby a strojírenství. Díky svému univerzálnímu strojovému vybavení a výkonným technologiím jsme schopni navrhnout, zkonstruovat a vyrobit strojírenské produkty vysoké kvality pro náročný evropský a světový trh.',
+    /* ms-sipervisor */
+    'ms-supervisor-title' => 'Dokonalý výhled pro operátora na CNC nástroj a obrobek během obrábění',
+    'ms-supervisor-text' => 'Odsávač aerosolů procesních kapalin Mestorm Solution zbaví vaši provozovnu olejové mlhy a problémů s ní souvisejících.',
+    'ms-supervisor-text1' => 'Supervisor je zařízení, vyvinuté společností Metales pod značkou Mestorm Solution, které slouží k řešení problémů spojených se špatnou viditelností při procesu obrábění, čímž chrání vaše zaměstnance proti zbytečnému hazardu se zdravím.',
+    'ms-supervisor-text2' => 'Zařízení, které výrazně přispívá k čistému pracovnímu prostředí v provozu s CNC stroji.',
+    'ms-supervisor-text3' => 'Zařízení Supervisor umisťujeme na dvířka CNC strojů, kde pomocí vzduchové trysky a jemně řízeného proudění vzduchu zabraňuje usazování kapalin na sklech. Viditelnost pracovní zóny stroje je díky tomu čistá a strojní proces je tak plně kontrolovatelný.',
+    'ms-supervisor-funkcia' => 'Jak zařízení funguje?',
+    'ms-supervisor-funkcia-text1' => 'Supervisor je systém vzduchových trysek a jemně řízeného proudění vzduchu, umístěný na dvířkách CNC stroje.',
+    'ms-supervisor-funkcia-text2' => 'Vzduchová tryska vytváří proudění vzduchu, které brání usazování kapalin na sklech dvířek, čímž zajišťuje perfektní viditelnost pracovního prostoru stroje.',
+    'ms-supervisor-funkcia-text3' => 'Zařízení je snadno instalovatelné a přizpůsobitelné pro různé typy CNC strojů.',
+    'ms-supervisor-benefity' => 'Benefity zařízení Supervisor',
+    'ms-supervisor-benefity-text1' => 'Zlepšená viditelnost pracovního prostoru CNC stroje',
+    'ms-supervisor-benefity-text2' => 'Ochrana zdraví zaměstnanců',
+    'ms-supervisor-benefity-text3' => 'Snadná instalace a údržba',
+    'ms-supervisor-benefity-text4' => 'Přizpůsobitelné pro různé typy CNC strojů',
+    'ms-supervisor-benefity-text5' => 'Dlouhodobá spolehlivost a efektivita',
+    /* ms-waste */
+    'ms-waste-title' => 'Inteligentní a snadná likvidace odpadu',
+    'ms-waste-text' => 'Odsávač aerosolů procesních kapalin Mestorm Solution zbaví vaši provozovnu olejové mlhy a problémů s ní souvisejících.',
+    'ms-waste-text1' => 'Zařízení Mestorm Solution pro likvidaci odpadu vám pomůže inteligentně a jednoduše řídit likvidaci odpadu v provozu, čímž zajistí čisté a bezpečné pracovní prostředí.',
+    'ms-waste-text2' => 'Naše zařízení jsou navržena tak, aby efektivně zvládala různé druhy odpadu, od kovových třísek až po olejové zbytky, a minimalizovala tak rizika spojená s jejich manipulací.',
+    'ms-waste-funkcia' => 'Jak zařízení funguje?',
+    'ms-waste-funkcia-text1' => 'Zařízení Mestorm Solution pro likvidaci odpadu je vybaveno inteligentními systémy pro třídění a zpracování odpadu.',
+    'ms-waste-funkcia-text2' => 'Efektivně sbírá a třídí odpad z různých částí provozu, čímž zajišťuje jeho správnou likvidaci a minimalizaci dopadu na životní prostředí.',
+    'ms-waste-funkcia-text3' => 'Zařízení je snadno ovladatelné a přizpůsobitelné pro různé typy provozů a druhy odpadu.',
+    'ms-waste-benefity' => 'Benefity zařízení pro likvidaci odpadu',
+    'ms-waste-benefity-text1' => 'Efektivní třídění a zpracování odpadu',
+    'ms-waste-benefity-text2' => 'Minimalizace rizik spojených s manipulací s odpadem',
+    'ms-waste-benefity-text3' => 'Snadná obsluha a údržba',
+    'ms-waste-benefity-text4' => 'Přizpůsobitelné pro různé typy provozů',
+    'ms-waste-benefity-text5' => 'Zajištění čistého a bezpečného pracovního prostředí',
+    /* ms-dust */
+    'ms-dust-title' => 'Odsávání prachu a nečistot z kompresoroven a technických místností',
+    'ms-dust-text' => 'Odsávač aerosolů procesních kapalin Mestorm Solution zbaví vaši provozovnu olejové mlhy a problémů s ní souvisejících.',
+    'ms-dust-text1' => 'Naše zařízení pro odsávání prachu a nečistot jsou navržena tak, aby efektivně odstraňovala prach a nečistoty z kompresoroven a technických místností, čímž zajistí čisté a bezpečné pracovní prostředí.',
+    'ms-dust-text2' => 'Zařízení jsou snadno instalovatelná a přizpůsobitelná pro různé typy technických místností a specifické potřeby provozu.',
+    'ms-dust-funkcia' => 'Jak zařízení funguje?',
+    'ms-dust-funkcia-text1' => 'Zařízení Mestorm Solution pro odsávání prachu a nečistot je vybaveno výkonnými filtračními systémy, které efektivně zachycují prach a nečistoty z ovzduší.',
+    'ms-dust-funkcia-text2' => 'Zařízení jsou navržena tak, aby byla snadno instalovatelná a přizpůsobitelná pro různé typy technických místností a specifické potřeby provozu.',
+    'ms-dust-funkcia-text3' => 'Zařízení jsou vybavena inteligentními systémy pro monitorování a řízení úrovně prachu a nečistot v ovzduší, čímž zajistí čisté a bezpečné pracovní prostředí.',
+    'ms-dust-benefity' => 'Benefity zařízení pro odsávání prachu a nečistot',
+    'ms-dust-benefity-text1' => 'Efektivní odsávání prachu a nečistot',
+    'ms-dust-benefity-text2' => 'Zajištění čistého a bezpečného pracovního prostředí',
+    'ms-dust-benefity-text3' => 'Snadná instalace a údržba',
+    'ms-dust-benefity-text4' => 'Přizpůsobitelné pro různé typy technických místností',
+    'ms-dust-benefity-text5' => 'Inteligentní systémy pro monitorování a řízení úrovně prachu a nečistot v ovzduší',
+];
+
+?>
